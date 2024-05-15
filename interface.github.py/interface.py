19056f85a1fc099f6722733552a25bf0d1fe2275
Nox <157261017+Soulsfuck@users.noreply.github.com>
2024-05-15 07:26:54 -0400

    Add files via upload

---
+++ interface.github.py/interface.py
@@ -0,0 +1,65 @@
+### Ola eu sou o ...
+
+
+<img src="COLOQUE A IMAGEM AQUI" alt="ilustração de um computador" min-width="400px" max-width="400px" width="400px" align="right">
+
+<p align="left"> 
+  Cybersecurity focado em pentest
+</p>
+
+<p align="left">
+  🦄 Linguagens: PYTHON
+</p>
+
+<p align="left">
+  💼 Ferramentas: Kali Linux
+</p>
+
+<p align="left">
+  Algum dos SO que eu utilizo: ⤵️
+</p>
+
+<p align="left">
+  <a href="#" title="LINUX">
+  <img src="COLOQUE O LINK DO BADGE AQUI"/></a>
+  <a href="" title="WINDOWS">
+  <img src="COLOQUE O LINK DO BADGE AQUI"/></a>
+</p>
+
+
+
+## Utilizaveis do dia dia <>
+
+<div style="display: inline_block"><br/>
+    <img align="center" alt="0101010101" src="COLOQUE O LINK DO BADGE AQUI" />
+    <img align="center" alt="0101010101" src="COLOQUE O LINK DO BADGE AQUI" />
+    <img align="center" alt="0101010101" src="COLOQUE O LINK DO BADGE AQUI" />
+    <img align="center" alt="0101010101" src="COLOQUE O LINK DO BADGE AQUI" />
+</div>
+
+### 🏆 GitHub Profile Trophy
+
+<p align="center">
+  <a
+    href="https://github.com/ryo-ma/github-profile-trophy"
+    title="repositório de troféus"
+  >
+    <img
+      width="800"
+      src="https://github-profile-trophy.vercel.app/?username=Soulsfuck&column=8&theme=darkhub&no-frame=true&no-bg=true"
+    />
+  </a>
+
+</p>
+
+---
+
+<div align="center">
+  <h3><b>📍 Perfil da minha conta</b></h3>
+</div>
+
+<p align="center">
+
+
+
+▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ 
